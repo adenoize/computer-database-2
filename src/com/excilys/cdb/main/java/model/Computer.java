ee9092c7615addeaa5810a04023e14b487f4d8d7
adenoize <adenoize@excilys.com>
2018-04-16 14:12:56 +0200

    creating model

---
+++ src/com/excilys/cdb/main/java/model/Computer.java
@@ -0,0 +1,199 @@
+package com.excilys.cdb.main.java.model;
+
+import java.util.Date;
+
+/**
+ * Class Computer.
+ * Represent all informations about computer.
+ * @author aurel
+ *
+ */
+public class Computer {
+	
+	private Long id;
+	private String name;
+	private Date introduced;
+	private Date discontinued;
+	private Long company;
+	
+	public Computer() {
+		super();
+	}
+	
+
+	/**
+	 * @param name
+	 */
+	public Computer(String name) {
+		super();
+		this.name = name;
+	}
+
+
+	/**
+	 * 
+	 * @param name
+	 * @param introduced
+	 * @param discontinued
+	 * @param company
+	 */
+	public Computer(String name, Date introduced, Date discontinued, Long company) {
+		super();
+		this.name = name;
+		this.introduced = introduced;
+		this.discontinued = discontinued;
+		this.company = company;
+	}
+
+
+	/**
+	 * @return the id
+	 */
+	public Long getId() {
+		return id;
+	}
+
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	/**
+	 * @return the introduced
+	 */
+	public Date getIntroduced() {
+		return introduced;
+	}
+
+
+	/**
+	 * @param introduced the introduced to set
+	 */
+	public void setIntroduced(Date introduced) {
+		this.introduced = introduced;
+	}
+
+
+	/**
+	 * @return the discontinued
+	 */
+	public Date getDiscontinued() {
+		return discontinued;
+	}
+
+
+	/**
+	 * @param discontinued the discontinued to set
+	 */
+	public void setDiscontinued(Date discontinued) {
+		this.discontinued = discontinued;
+	}
+
+
+	/**
+	 * @return the company
+	 */
+	public Long getCompany() {
+		return company;
+	}
+
+
+	/**
+	 * @param company the company to set
+	 */
+	public void setCompany(Long company) {
+		this.company = company;
+	}
+
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((company == null) ? 0 : company.hashCode());
+		result = prime * result + ((discontinued == null) ? 0 : discontinued.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((introduced == null) ? 0 : introduced.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Computer other = (Computer) obj;
+		if (company == null) {
+			if (other.company != null)
+				return false;
+		} else if (!company.equals(other.company))
+			return false;
+		if (discontinued == null) {
+			if (other.discontinued != null)
+				return false;
+		} else if (!discontinued.equals(other.discontinued))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (introduced == null) {
+			if (other.introduced != null)
+				return false;
+		} else if (!introduced.equals(other.introduced))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
+
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "Computer [id=" + id + ", name=" + name + ", introduced=" + introduced + ", discontinued=" + discontinued
+				+ ", company=" + company + "]";
+	}
+	
+	
+	
+	
+
+}
